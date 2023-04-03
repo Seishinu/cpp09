4f3873dad5047e217c157ff4df1f8fd8c44b9980
Yassine Nuiga <ynuiga@e1r1p3.1337.ma>
2023-04-03 17:59:49 +0000

    RPN SEGV protection

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ynuiga <ynuiga@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 14:09:17 by ynuiga            #+#    #+#             */
-/*   Updated: 2023/04/02 14:30:44 by ynuiga           ###   ########.fr       */
+/*   Updated: 2023/04/03 17:41:59 by ynuiga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int ac, char **av)
 {
-	if (ac == 2) {
+	if (ac == 2 && av[1][0]) {
 		RPN myrpn((std::string)av[1]);
 		return 0;
 	}
